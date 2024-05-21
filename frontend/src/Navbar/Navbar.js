9bef827c8e8c75f24c15d120850a49259786ee14
msuudoct2@gmail.com <msuudoct2@gmail.com>
2024-05-21 14:53:19 +0530

    dashboard-half

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,22 +1,38 @@
 import React from "react";
-import { NavLink } from 'react-router-dom'
-import './Navbar.css'; 
-import HomeIcon from '@mui/icons-material/Home';
-import BadgeIcon from '@mui/icons-material/Badge';
-import PersonIcon from '@mui/icons-material/Person';
-import InfoIcon from '@mui/icons-material/Info';
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import HomeIcon from "@mui/icons-material/Home";
+import BadgeIcon from "@mui/icons-material/Badge";
+import PersonIcon from "@mui/icons-material/Person";
+import InfoIcon from "@mui/icons-material/Info";
 
 const navigationItems = [
-  { name: 'Dashboard', path: '/Dashboard',icon:<HomeIcon className="nav-icon"/> },
-  { name: 'Workspace', path: '/workspace', icon:<BadgeIcon className="nav-icon"/>},
-  { name: 'Profile', path: '/profile', icon:<PersonIcon className="nav-icon"/> },
-  { name: 'About Us', path: '/about-us', icon:<InfoIcon className="nav-icon"/> },
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: <HomeIcon className="nav-icon" />,
+  },
+  {
+    name: "Workspace",
+    path: "/workspace",
+    icon: <BadgeIcon className="nav-icon" />,
+  },
+  {
+    name: "Profile",
+    path: "/profile",
+    icon: <PersonIcon className="nav-icon" />,
+  },
+  {
+    name: "About Us",
+    path: "/about-us",
+    icon: <InfoIcon className="nav-icon" />,
+  },
 ];
 
 const Navbar = () => {
-  return  ( 
-<nav className="vertical-navbar">
-<div className="company-logo">OptiSnap</div>
+  return (
+    <nav className="vertical-navbar">
+      <div className="company-logo">OptiSnap</div>
       <ul>
         {navigationItems.map((item) => (
           <li key={item.name}>
@@ -28,7 +44,7 @@ const Navbar = () => {
         ))}
       </ul>
     </nav>
-);
+  );
 };
 
 export default Navbar;
