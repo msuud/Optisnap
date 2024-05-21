05c9f78308867259e5c297b117ea1464ca1bad95
Tanvidkp05 <tanvidkp05@gmail.com>
2024-05-21 12:02:44 +0530

    navbar

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { NavLink } from 'react-router-dom'
+import './Navbar.css'; 
+import HomeIcon from '@mui/icons-material/Home';
+import BadgeIcon from '@mui/icons-material/Badge';
+import PersonIcon from '@mui/icons-material/Person';
+import InfoIcon from '@mui/icons-material/Info';
+
+const navigationItems = [
+  { name: 'Dashboard', path: '/Dashboard',icon:<HomeIcon className="nav-icon"/> },
+  { name: 'Workspace', path: '/workspace', icon:<BadgeIcon className="nav-icon"/>},
+  { name: 'Profile', path: '/profile', icon:<PersonIcon className="nav-icon"/> },
+  { name: 'About Us', path: '/about-us', icon:<InfoIcon className="nav-icon"/> },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return  ( 
+<nav className="vertical-navbar">
+<div className="company-logo">OptiSnap</div>
+      <ul>
+        {navigationItems.map((item) => (
+          <li key={item.name}>
+            <NavLink to={item.path} activeClassName="active">
+              {item.icon}
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+);
 };
 
 export default Navbar;
