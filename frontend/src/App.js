2741f9ffcdd67ecb3fe395faa3641788a003c85f
msuudoct2@gmail.com <msuudoct2@gmail.com>
2024-05-22 16:59:56 +0530

    login.js file done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,29 +6,29 @@ import Dashboard from "./Dashboard/Dashboard";
 import AboutUs from "./About-us/aboutus";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-  useEffect(() => {
-    // Check if the user is logged in when the component mounts
-    const user = localStorage.getItem("user");
-    if (user) {
-      setIsLoggedIn(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   // Check if the user is logged in when the component mounts
+  //   const user = localStorage.getItem("user");
+  //   if (user) {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
-        {isLoggedIn ? (
-          <div className="main-layout">
+        {console.log(isLoggedIn) ? (
+          <div className="">
             <div className="navbar">
               <Navbar />
-            </div>
-            <div className="dashboard">
-              <Routes>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/about-us" element={<AboutUs />} />
-              </Routes>
+              <div className="dashboard content-container">
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/about-us" element={<AboutUs />} />
+                </Routes>
+              </div>
             </div>
           </div>
         ) : (
