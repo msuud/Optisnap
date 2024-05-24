3c00b7121af7acfe26f46ad24039b2099c6caa38
Tanvidkp05 <tanvidkp05@gmail.com>
2024-05-24 19:19:03 +0530

    animations for login/sign-up and verification form is done and rendering is also done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
-import Login from "./Login/Login";
 import Dashboard from "./Dashboard/Dashboard";
 import AboutUs from "./About-us/aboutus";
+import SignupForm from "./Authorization/SignupForm";
+import LoginForm from "./Authorization/LoginForm";
+import VerificationForm from "./Authorization/VerificationForm";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check if the user is logged in when the component mounts
-    const user = localStorage.getItem("user");
-    if (user) {
+    const client = localStorage.getItem("client");
+    if (client) {
       setIsLoggedIn(true);
     }
   }, []);
@@ -21,19 +22,25 @@ function App() {
       <BrowserRouter>
         {isLoggedIn ? (
           <div>
-            <div className="d-flex flex-row min-vh-100 page-cointainer">
+            <div className="d-flex flex-row min-vh-100 ">
               <Navbar />
-            <div className="container-fluid dashboard content-cointainer mt-0 px-0 content">
-              <Routes>
-                <Route path="/dashboard" element={<Dashboard />} className="dashboard" />
-                <Route path="/about-us" element={<AboutUs />}  className="dashboard"/>
-              </Routes>
+              <div className="container-fluid dashboard content-cointainer mt-0 px-0">
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/about-us" element={<AboutUs />} />
+                </Routes>
+              </div>
             </div>
-          </div>
           </div>
         ) : (
           <Routes>
-            <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+            <Route
+              path="/login"
+              element={<LoginForm setIsLoggedIn={setIsLoggedIn} />}
+            />{" "}
+            <Route path="/" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/verification" element={<VerificationForm />} />
           </Routes>
         )}
       </BrowserRouter>
@@ -41,4 +48,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
