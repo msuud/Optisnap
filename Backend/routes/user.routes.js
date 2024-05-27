afbe3b50e36cdc4be5e4fd9ca9c28c75644df7f8
Bhavya <bilimoriabhavya1@gmail.com>
2024-05-27 22:52:55 +0530

    changes backend to bhavya and frontend to urva

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -9,38 +9,41 @@ const { StatusCodes } = require('http-status-codes');
 const authenticateToken = require('../middleware/authenticateToken')
 const router = express.Router();
 router.use(express.json())
+router.use(cookieParser())
 
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 const transport = nodemailer.createTransport({
-  service: "gmail",
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL,
-    pass: process.env.PASS  // Use App Password if applicable
+    pass: process.env.PASS
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     let body = req.body;
     let u = await user.findOne({ email: body.email });
-    if (!u.verified) {
-      res.send("Please verify the email before logging in.").status(StatusCodes.UNAUTHORIZED)
+    if (u == null) {
+      res.send("Email is not registered").status(StatusCodes.UNAUTHORIZED)
+
+    }
+    else if (!u.verified) {
+      res.send("Email is not verified").status(StatusCodes.UNAUTHORIZED)
     }
     else if (await bcrypt.compare(body.password, u.password)) {
       const email = { name: body.email }
-      console.log(email);
+      // console.log(email);
       const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET)
+      res.cookie('accessToken', accessToken, { httpOnly: true })
       res.json({
         message: 'Authentication Successful',
-        accessToken: accessToken
       }).status(StatusCodes.ACCEPTED);
     } else {
-      res
-        .json({
-          message: "Authentication Failed",
-        })
-        .status(StatusCodes.NOT_FOUND);
+      res.json({
+        message: 'Authentication Failed'
+      }).status(StatusCodes.NOT_FOUND);
     }
   } catch (error) {
     console.log(error);
@@ -49,38 +52,34 @@ router.post("/login", async (req, res) => {
 });
 
 const sendVerificationEmail = async ({ _id, email }, res) => {
-  const currentURL = "http:localhost:4000/";
+  const currentURL = "http:localhost:4000/"
   const uniqueString = uuidv4() + _id;
   const mailOptions = {
     from: process.env.AUTH_EMAIL,
     to: email,
     subject: "Verify Your Email",
     html: `<p>Verify your email address to complete the signup and login into your account .< /p><p>This link <b>expires in 6 hours</b> .
-      < /p><p>Press <a href=${
-        currentURL + "verify/" + _id + "/" + uniqueString
-      }>here</a> to proceed .< /p>`,
-  };
+      < /p><p>Press <a href=${currentURL + "verify/" + _id + "/" + uniqueString}>here</a> to proceed .< /p>`,
+  }
 
   try {
-    const hashedUniqueString = await bcrypt.hash(uniqueString, 10);
+    const hashedUniqueString = await bcrypt.hash(uniqueString, 10)
     // console.log(hashedUniqueString);
     const newUserVerification = new userVerification({
       uid: _id,
       uString: hashedUniqueString,
       createdAt: Date.now(),
       expiresAt: Date.now() + 21600000,
-    });
-    await newUserVerification.save();
-    await transport.sendMail(mailOptions);
-    return "mail sent!!";
+    })
+    await newUserVerification.save()
+    await transport.sendMail(mailOptions)
+    return "mail sent!!"
   } catch (error) {
-    res
-      .send("Error at sending the email.")
-      .status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.send("Error at sending the email.").status(StatusCodes.INTERNAL_SERVER_ERROR)
   }
-};
+}
 
-router.post("/signup", async (req, res) => {
+router.post('/signup', async (req, res) => {
   try {
     const body = req.body;
     const hashedPassword = await bcrypt.hash(body.password, 10);
@@ -92,30 +91,36 @@ router.post("/signup", async (req, res) => {
       verified: false,
     });
     let response = await u.save();
-    response = await sendVerificationEmail(response, res);
-    res
-      .json({
-        message: "User Created",
-        response: response,
-      })
-      .status(StatusCodes.CREATED);
+    response = await sendVerificationEmail(response, res)
+    res.json({
+      message: "User Created",
+      response: response
+    }).status(StatusCodes.CREATED);
   } catch (error) {
     console.log(error);
-    res.send(error.message).status(StatusCodes.INTERNAL_SERVER_ERROR);
+    if (error.code === 11000) {
+      // console.log();
+      if (error.message.search("username") !== -1) {
+        res.send("username exist").status(StatusCodes.BAD_REQUEST)
+      }
+      else {
+        res.send("Email exists").status(StatusCodes.BAD_REQUEST)
+      }
+    }
+    else {
+      res.send(error.message).status(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
   }
 });
 
-router.patch("/update", async (req, res) => {
+router.patch('/update', async (req, res) => {
   try {
     const body = req.body;
-    const u = await user.updateOne(
-      {
-        email: body.email,
-      },
-      {
-        $set: body,
-      }
-    );
+    const u = await user.updateOne({
+      email: body.email
+    }, {
+      $set: body
+    });
     console.log(u);
     if (u.acknowledged) {
       res.send("Updated successfully").status(StatusCodes.OK);
@@ -131,13 +136,13 @@ router.patch("/update", async (req, res) => {
 router.get('/', authenticateToken, (req, res) => {
   res.json({ user: req.user })
 })
-module.exports = router;
 
 router.post("/logout", authenticateToken, async (req, res) => {
   try {
-    await user.findOneAndDelete({ email: res.user.name })
-    res.json({ message: "Logged Out" }).status(StatusCodes.OK);
+    res.clearCookie("accessToken").send("logged out");
   } catch (error) {
-    res.send(error.message).status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.send(error.message).status(StatusCodes.INTERNAL_SERVER_ERROR)
   }
 })
+
+module.exports = router;
