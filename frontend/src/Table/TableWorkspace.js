7279cbcb609e263abe38ea3c57decf16d6447a0b
msuudoct2@gmail.com <msuudoct2@gmail.com>
2024-05-29 17:36:44 +0530

    merged from tanvi

---
+++ frontend/src/Table/TableWorkspace.js
@@ -0,0 +1,109 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(image, size, Uploaddate, LastUsed) {
+  return { image, size, Uploaddate, LastUsed };
+}
+
+const rows = [
+  createData("image 1", "450MB", "21 December 2023", "20 March 2024"),
+  createData("image 2", "700MB", "2 December 2023", "22 December 2023"),
+  createData("image 3", "1GB", "3 January 2023", "2 February 2024"),
+  createData("image 4", "210MB", "22 May 2023", "4 January 2023"),
+];
+
+export default function BasicTable() {
+  return (
+    <div className="table">
+      <TableContainer
+        component={Paper}
+        style={{
+          boxShadow: "50px 50px 60px 20px #80808029",
+          borderRadius: "20px",
+          width: "1000px",
+          alignSelf: "center",
+        }}
+      >
+        <Table
+          sx={{ tableLayout: "fixed", borderRadius: "20px" }}
+          aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell
+                align="center"
+                style={{ fontSize: "20px", color: "green", fontWeight: "bold" }}
+              >
+                Image
+              </TableCell>
+              {/* <TableCell align="left">Name</TableCell>
+              <TableCell align="left">URL</TableCell> */}
+              <TableCell
+                align="center"
+                style={{ fontSize: "20px", color: "green", fontWeight: "bold" }}
+              >
+                Size
+              </TableCell>
+              <TableCell
+                align="center"
+                style={{ fontSize: "20px", color: "green", fontWeight: "bold" }}
+              >
+                Upload Date
+              </TableCell>
+              <TableCell
+                align="center"
+                style={{ fontSize: "20px", color: "green", fontWeight: "bold" }}
+              >
+                Last Updated
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell
+                  align="center"
+                  component="th"
+                  scope="row"
+                  style={{ fontSize: "17px" }}
+                >
+                  {row.image}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  component="th"
+                  scope="row"
+                  style={{ fontSize: "17px" }}
+                >
+                  {row.size}
+                </TableCell>
+                <TableCell
+                  align="center"
+                  component="th"
+                  scope="row"
+                  style={{ fontSize: "17px" }}
+                >
+                  {row.Uploaddate}
+                </TableCell>
+                {/* <TableCell align="left">{row.name}</TableCell>
+                <TableCell align="left">{row.link}</TableCell> */}
+                <TableCell align="center" style={{ fontSize: "17px" }}>
+                  {row.LastUsed}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
---
+++ frontend/src/Workspace/WorkspaceDetails.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./WorkspaceDetails.css";
+import TableWorkspace from "../Table/TableWorkspace";
+
+const WorkspaceDetails = ({ data }) => {
+  const { id } = useParams();
+  const [workspace, setWorkspace] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      const foundWorkspace = data.find((item) => parseInt(id) === item.id);
+      setWorkspace(foundWorkspace);
+    }
+  }, [data, id]);
+
+  if (!workspace) {
+    return <div>Workspace not found!</div>;
+  }
+
+  return (
+    <div className="bg-image1 rounded d-flex flex-column ">
+      <div className="grid1w rounded fixed top-0 left-0 right-0 z-10 text-center p-5">
+        <h1 className="workspace-heading">Workspace Details</h1>
+        <div className="cointainer-workspace mt-4">
+          <h2 className="">Workspace Name: {workspace.title}</h2>
+          <h2 className="">Images: {workspace.images}</h2>
+          <div className="d-flex justify-content-center mt-4">
+            <div className="Table-workspace">
+              <div className="d-flex justify-content-start my-2 mx-3 upload-button">
+                <button className="text-start">Upload Image</button>
+              </div>
+              <TableWorkspace />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkspaceDetails;
